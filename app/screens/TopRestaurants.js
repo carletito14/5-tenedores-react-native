3143845e87916280a0bd5e17251cac83ae179c60
carletito14 <contactomanuelgutierrez@gmail.com>
2021-11-26 13:40:35 +0100

    rating

--- app/screens/TopRestaurants.js
+++ app/screens/TopRestaurants.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { View, Text } from 'react-native'
+import Toast from 'react-native-easy-toast';
+import ListTopRestaurants from '../components/Ranking/ListTopRestaurants';
 
-const TopRestaurants = () => {
+import { firebaseApp } from "../utils/firebase";
+import firebase from "firebase";
+import "firebase/firestore";
+
+const db = firebase.firestore(firebaseApp);
+
+const TopRestaurants = ({ navigation }) => {
+
+    const [restaurants, setRestaurants] = useState([])
+    const toastRef = useRef()
+
+    useEffect(() => {
+        db.collection('restaurants')
+            .orderBy("rating", "desc")
+            .limit(5)
+            .get()
+            .then((res) => {
+                const restaurantArray = []
+                res.forEach((doc) => {
+                    const data = doc.data()
+                    data.id = doc.id
+                    restaurantArray.push(data)
+                });
+                setRestaurants(restaurantArray)
+            })
+    }, [])
     return (
         <View>
-            <Text>TopRestaurants...</Text>
+            <ListTopRestaurants restaurants={restaurants} navigation={navigation} />
+            <Toast ref={toastRef} position="center" opacity={0.9} />
         </View>
     )
 }
